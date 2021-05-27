60d9fc3ac025ce788edeca8e8a98479fb35a5a79
Rubenval <menezesruben1972@gmail.com>
2021-05-27 17:58:39 -0300

    Dashboard (bar e donut)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -7,6 +9,20 @@ function App() {
     <>
       <NavBar />
       <div className="container">
+        <h1 className="text-primary py-3">Dashboar de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5>Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5>Todas Vendas</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+
         <DataTable />
       </div>
       <Footer />
