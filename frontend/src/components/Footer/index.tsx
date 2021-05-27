b0b946f441710ff38122017de4482e5f5c76520b
Rubenval <menezesruben1972@gmail.com>
2021-05-27 17:03:59 -0300

    Frontend

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rubenval" target="_blank" rel="noreferrer">Rubenval</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/rubenvalmenezes" target="_blank" rel="noreferrer">@rubenvalmenezes</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
